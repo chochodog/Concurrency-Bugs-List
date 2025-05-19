e2f923b45bab1008ac06ffe5b33d7d084f808c10
chuck-e- <126799413+chochodog@users.noreply.github.com>
2025-05-19 15:20:02 +0900

    Update s3.py

--- short_codes/Starvation/s3.py
+++ short_codes/Starvation/s3.py
@@ -5,13 +5,11 @@ def resource_user(thread_id, hold_time):
     while True:
         with resource_lock:
             print(f"Thread {thread_id} is using the resource.")
-            time.sleep(hold_time)  # 자원을 사용하는 시간
+            time.sleep(hold_time)
             print(f"Thread {thread_id} released the resource.")
 
 resource_lock = threading.Lock()
-# 스레드 1은 자원을 0.5초 동안 사용
 thread1 = threading.Thread(target=resource_user, args=(1, 0.5))
-# 스레드 2는 자원을 0.1초 동안만 사용
 thread2 = threading.Thread(target=resource_user, args=(2, 0.1))
 
 thread1.start()
