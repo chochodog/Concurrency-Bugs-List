a2c54824a8c0e8162286709c2587c6ac1c0d47b9
chuck-e- <126799413+chochodog@users.noreply.github.com>
2025-05-19 15:18:15 +0900

    Update r5.cpp

--- short_codes/RaceCondiotion/r5.cpp
+++ short_codes/RaceCondiotion/r5.cpp
@@ -2,30 +2,25 @@
 #include <thread>
 #include <vector>
 
-// 공유 변수
 int counter = 0;
 
-// 스레드가 실행할 함수: counter 변수 증가
 void increment() {
     for (int i = 0; i < 10000; ++i) {
-        ++counter;  // 공유 변수 수정
+        ++counter;
     }
 }
 
 int main() {
     std::vector<std::thread> threads;
 
-    // 10개의 스레드 생성
     for (int i = 0; i < 10; ++i) {
         threads.emplace_back(increment);
     }
 
-    // 모든 스레드가 종료될 때까지 대기
     for (auto& th : threads) {
         th.join();
     }
 
-    // 결과 출력
     std::cout << "Expected counter value: 100000\n";
     std::cout << "Actual counter value: " << counter << std::endl;
 
