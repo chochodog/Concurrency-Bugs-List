5e25223c6f2ab17715ed7b6f960f118d27095a1e
chuck-e- <126799413+chochodog@users.noreply.github.com>
2025-05-19 10:49:08 +0900

    Update d3.py

--- short_codes/DeadLock/d3.py
+++ short_codes/DeadLock/d3.py
@@ -7,9 +7,9 @@ def task():
     print("Trying to acquire semaphore...")
     sem.acquire()
     print("Semaphore acquired")
-    time.sleep(2)  # 임의의 작업
+    time.sleep(2)
     print("Trying to acquire semaphore again...")  
-    sem.acquire()  # 이미 획득한 세마포어를 다시 획득하려 함 -> 데드락 발생
+    sem.acquire()
     print("Semaphore acquired again (never reached)")
     sem.release()
     sem.release()
